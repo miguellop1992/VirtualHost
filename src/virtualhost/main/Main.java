f6e9f5a58001ea00f31049eb7146d89084f38379
Miguel lopez <developer@miguellop.com.ve>
2015-12-29 23:07:19 -0430

    add icon

--- src/virtualhost/main/Main.java
+++ src/virtualhost/main/Main.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -22,7 +23,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/virtualhost/view/main.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/virtualhost/icon/host.jpg")));
         stage.setScene(scene);
         stage.show();
     }
